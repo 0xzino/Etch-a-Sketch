ef082461757a03130dff57f553e3e79ee4bb3e21
Zino <nizarcarjoun@gmail.com>
2026-02-04 19:58:14 +0800

    Refactored code for flexbox only requirement.

--- javascript.js
+++ javascript.js
@@ -3,9 +3,9 @@ function createGrid(size = 16) {
     
     for (let i = 0; i < size * size; i++) {
         const div = document.createElement('div');
-        div.classList.add('grid-box');
+        div.classList.add('box');
         container.appendChild(div);
     }
 }
 
-createGrid();
+createGrid(16);
